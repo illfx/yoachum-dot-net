1b00c1df26bd42b27eb94d261ea9784a8bc45254
Stephen Yoachum <spam@yoachum.net>
2018-03-09 08:17:48 -0500

    Implement's a basic User/Role ACL foundation

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+
+class UserTableSeeder extends Seeder
+{
+    
+    public function run()
+    {
+        $role = Role::where('name', 'super')->first();
+
+        $user = new User();
+        $user->name = env('APP_ADMIN_NAME');
+        $user->email = env('APP_ADMIN_EMAIL');
+        $user->password = bcrypt(env('APP_ADMIN_SECRET'));
+        $user->save();
+        $user->roles()->attach($role);
+
+    }
+  
+}
